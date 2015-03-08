e09bbbbfefa4ecdeaffdfa805eaaa8f56aed333f
Villena Guillaume <contact@guillaumevillena.fr>
2015-03-08 14:57:55 +0100

    Getting user config

--- app/src/main/java/net/dgistudio/guillaume/opendnsupdater/onNetworkChange.java
+++ app/src/main/java/net/dgistudio/guillaume/opendnsupdater/onNetworkChange.java
@@ -3,17 +3,28 @@ package net.dgistudio.guillaume.opendnsupdater;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
@@ -23,36 +34,35 @@ public class onNetworkChange extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        /*String action = intent.getAction();
-        if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-            ConnectivityManager conMan = (ConnectivityManager) context
-                    .getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = conMan.getActiveNetworkInfo();
-            if (netInfo != null) {
-                if (netInfo.getType() == ConnectivityManager.TYPE_ETHERNET) {
-                    // is connected
-                }
-            }
-        }*/
 
         Log.d("BroadCast", "Change");
-        Authenticator.setDefault(new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("guiguivil@gmail.com", "00124500gvGV".toCharArray());
-            }
-        });
-        HttpURLConnection c = null;
-        try {
-            c = (HttpURLConnection) new URL("https://updates.opendns.com/nic/update?hostname=Home").openConnection();
-            c.setUseCaches(false);
-            c.connect();
 
-            Log.d("result", c.getResponseMessage());
-            Log.d("result",""+c.getResponseCode());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
+        String username  = prefs.getString("OpenDns_Username","");
+        String password  = prefs.getString("OpenDns_Password","");
 
+        Log.d("pref",""+username);
+        Log.d("pref",""+password);
+
+        /*try{
+           HttpClient httpclient = new DefaultHttpClient();
+
+           HttpGet request = new HttpGet();
+           URI website = new URI("http://alanhardin.comyr.com/matt24/matt28.php");
+           request.setURI(website);
+           HttpResponse response = httpclient.execute(request);
+           in = new BufferedReader(new InputStreamReader(
+                   response.getEntity().getContent()));
+
+           // NEW CODE
+           String line = in.readLine();
+           textv.append(" First line: " + line);
+           // END OF NEW CODE
+
+           textv.append(" Connected ");
+       }catch(Exception e){
+           Log.e("log_tag", "Error in http connection "+e.toString());
+       }*/
     }
 }

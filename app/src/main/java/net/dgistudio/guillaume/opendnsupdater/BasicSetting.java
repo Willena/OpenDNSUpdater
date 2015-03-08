738744a9173c00e8dc00f0d650113d9a077b0d16
Villena Guillaume <contact@guillaumevillena.fr>
2015-03-08 22:03:04 +0100

    code optimisations

--- app/src/main/java/net/dgistudio/guillaume/opendnsupdater/BasicSetting.java
+++ app/src/main/java/net/dgistudio/guillaume/opendnsupdater/BasicSetting.java
@@ -1,24 +1,7 @@
 package net.dgistudio.guillaume.opendnsupdater;
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
-
-
-import java.util.List;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
--- app/src/main/java/net/dgistudio/guillaume/opendnsupdater/onNetworkChange.java
+++ app/src/main/java/net/dgistudio/guillaume/opendnsupdater/onNetworkChange.java
@@ -2,7 +2,6 @@ package net.dgistudio.guillaume.opendnsupdater;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,13 +11,13 @@ import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+
 import java.net.URI;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -35,70 +34,69 @@ public class onNetworkChange extends BroadcastReceiver {
 
         //Log.d("BroadCast", "Change");
 
-        ConnectivityManager connMgr = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-        Log.d("info", "We are connected");
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            Log.d("info", "We are connected");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        final String username  = prefs.getString("OpenDns_Username", "");
-        final String password  = prefs.getString("OpenDns_Password", "");
+            final String username = prefs.getString("OpenDns_Username", "");
+            final String password = prefs.getString("OpenDns_Password", "");
 
-        //Log.d("pref",""+username);
-        //Log.d("pref",""+password);
+            //Log.d("pref",""+username);
+            //Log.d("pref",""+password);
 
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-                    try{
-                        HttpClient httpclient = new DefaultHttpClient();
-                        HttpGet request = new HttpGet();
-                        URI website = new URI("https://updates.opendns.com/nic/update?hostname=Home");
-                        request.setHeader("Authorization", "Basic " + Base64.encodeToString((username + ":" + password).getBytes("UTF-8"), Base64.NO_WRAP));
-                        request.setURI(website);
-                        HttpResponse response = httpclient.execute(request);
-                        String result = EntityUtils.toString(response.getEntity());
-                        Log.d("res", result);
-                        if (result.split(" ")[0].equals("good"))
-                        {
-                            final NotificationManager mNotification = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        try {
+                            HttpClient httpclient = new DefaultHttpClient();
+                            HttpGet request = new HttpGet();
+                            URI website = new URI("https://updates.opendns.com/nic/update?hostname=Home");
+                            request.setHeader("Authorization", "Basic " + Base64.encodeToString((username + ":" + password).getBytes("UTF-8"), Base64.NO_WRAP));
+                            request.setURI(website);
+                            HttpResponse response = httpclient.execute(request);
+                            String result = EntityUtils.toString(response.getEntity());
+                            Log.d("res", result);
+                            if (result.split(" ")[0].equals("good")) {
+                                final NotificationManager mNotification = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
 
-                          //  final Intent launchNotifiactionIntent = new Intent(this, TutoNotificationHomeActivity.class);
-                            //final PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                                   // context.REQUEST_CODE, launchNotifiactionIntent,
-                                   // PendingIntent.FLAG_ONE_SHOT);
+                                //  final Intent launchNotifiactionIntent = new Intent(this, TutoNotificationHomeActivity.class);
+                                //final PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                                // context.REQUEST_CODE, launchNotifiactionIntent,
+                                // PendingIntent.FLAG_ONE_SHOT);
 
-                            Notification.Builder builder = new Notification.Builder(context)
-                                    .setWhen(System.currentTimeMillis())
-                                    .setTicker("Yeah")
-                                    .setSmallIcon(R.drawable.ic_launcher)
-                                    .setContentTitle("holé")
-                                    .setContentText("holaaaaaaa !");
+                                Notification.Builder builder = new Notification.Builder(context)
+                                        .setWhen(System.currentTimeMillis())
+                                        .setTicker("Yeah")
+                                        .setSmallIcon(R.drawable.ic_launcher)
+                                        .setContentTitle("holé")
+                                        .setContentText("holaaaaaaa !");
 
-                            mNotification.notify(0, builder.build());
+                                mNotification.notify(0, builder.build());
 
-                            Runnable task = new Runnable() {
-                                public void run() {
-                                    mNotification.cancel(0);
-                                }
-                            };
-                            final ScheduledExecutorService worker =  Executors.newSingleThreadScheduledExecutor();
-                            worker.schedule(task, 5, TimeUnit.SECONDS);
+                                Runnable task = new Runnable() {
+                                    public void run() {
+                                        mNotification.cancel(0);
+                                    }
+                                };
+                                final ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
+                                worker.schedule(task, 5, TimeUnit.SECONDS);
+                            }
+
+                        } catch (Exception e) {
+                            Log.e("log_tag", "Error in http connection " + e.toString());
                         }
-
-                    }catch(Exception e){
-                        Log.e("log_tag", "Error in http connection " + e.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-            }
-        });
+            });
 
-        thread.start();
+            thread.start();
 
 
-    }
+        }
     }
 }

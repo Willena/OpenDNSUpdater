d5008dda4a20766d057ed8287d7c531b989d9aa7
Villena Guillaume <contact@guillaumevillena.fr>
2018-12-24 01:53:40 +0100

    Fixed shortcut bug

--- app/src/main/java/fr/guillaumevillena/opendnsupdater/activity/ActivityEnableDisableVPNService.java
+++ app/src/main/java/fr/guillaumevillena/opendnsupdater/activity/ActivityEnableDisableVPNService.java
@@ -2,29 +2,37 @@ package fr.guillaumevillena.opendnsupdater.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import fr.guillaumevillena.opendnsupdater.OpenDnsUpdater;
 
 public class ActivityEnableDisableVPNService extends AppCompatActivity {
 
-    public static final String LAUNCH_ACTION = "fr.guillaumevillena.opendnsupdater.activityenabledisablevpnservice.launch.action";
-    public static final String LAUNCH_ACTION_DEACTIVATE = "fr.guillaumevillena.opendnsupdater.activityenabledisablevpnservice.launch.action.deactivate";
-    public static final String LAUNCH_ACTION_ACTIVATE = "fr.guillaumevillena.opendnsupdater.activityenabledisablevpnservice.launch.action.activate";
-    public static final String LAUNCH_ACTION_TOGGLE = "fr.guillaumevillena.opendnsupdater.activityenabledisablevpnservice.launch.action.toggle";
+    public static final String LAUNCH_ACTION = "fr.guillaumevillena.opendnsupdater.activity.ActivityEnableDisableVPNService.LAUNCH_ACTION";
+    public static final String LAUNCH_ACTION_DEACTIVATE = "fr.guillaumevillena.opendnsupdater.activity.ActivityEnableDisableVPNService.launch.action.deactivate";
+    public static final String LAUNCH_ACTION_ACTIVATE = "fr.guillaumevillena.opendnsupdater.activity.ActivityEnableDisableVPNService.launch.action.activate";
+    public static final String LAUNCH_ACTION_TOGGLE = "fr.guillaumevillena.opendnsupdater.activity.ActivityEnableDisableVPNService.launch.action.toggle";
+    private static final String TAG = ActivityEnableDisableVPNService.class.getSimpleName();
+    private Intent incoming;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent i = getIntent();
+        this.incoming = getIntent();
 
-        if (i.getExtras() == null) {
+        String toto = this.incoming.getStringExtra(LAUNCH_ACTION);
+
+        Log.d(TAG, "onCreate: Receievd ! " + toto + " ");
+
+        if (toto == null) {
             finish();
             return;
         }
 
-        String action = i.getExtras().getString(LAUNCH_ACTION);
+
+        String action = incoming.getStringExtra(LAUNCH_ACTION);
         if (action != null) {
             if (action.equals(LAUNCH_ACTION_ACTIVATE)) {
                 OpenDnsUpdater.activateService(this);
